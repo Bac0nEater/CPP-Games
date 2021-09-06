9c755d74f3962064d1a534489ad8c7899ab165ed
Bac0nEater <69305722+Bac0nEater@users.noreply.github.com>
2021-09-06 18:04:54 +1200

    Found two small bugs

--- TWL/TWL/code/Thomas.h
+++ TWL/TWL/code/Thomas.h
@@ -5,7 +5,7 @@ class Thomas : public PlayableCharacter
 {
 public:
 	// A constructor specific to Thomas
-	Thomas::Thomas();
+	Thomas();
 
 	// The overridden input handler for Thomas
 	virtual bool handleInput();
