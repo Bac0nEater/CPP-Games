52406a768e9fa50f938702758cd697f5ab0f8358
Bac0nEater <69305722+Bac0nEater@users.noreply.github.com>
2021-09-07 17:56:23 +1200

    Update Engine.h

--- TWL/TWL/code/Engine.h
+++ TWL/TWL/code/Engine.h
@@ -3,6 +3,7 @@
 #include "TextureHolder.h"
 #include "Thomas.h"
 #include "Bob.h"
+#include "LevelManager.h"
 
 using namespace sf;
 
@@ -15,6 +16,9 @@ private:
 	// Thomas and his friend, Bob
 	Thomas m_Thomas;
 	Bob m_Bob;
+
+	// A class to manage all the levels
+	LevelManager m_LM;
 
 	const int TILE_SIZE = 50;
 	const int VERTS_IN_QUAD = 4;
@@ -58,11 +62,24 @@ private:
 	// Is it time for a new/first level?
 	bool m_NewLevelRequired = true;
 
+	// The vertex array for the level tiles
+	VertexArray m_VALevel;
+
+	// The 2d array with the map for the level
+	// A pointer to a pointer
+	int** m_ArrayLevel = NULL;
+
+	// Texture for the level tiles
+	Texture m_TextureTiles;
+
 
 	// Private functions for internal use only
 	void input();
 	void update(float dtAsSeconds);
 	void draw();
+
+	// Load a new level
+	void loadLevel();
 
 public:
 	// The Engine constructor
