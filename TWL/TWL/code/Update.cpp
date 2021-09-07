daed75f87b20705d61b05e165c265543911ad966
Bac0nEater <69305722+Bac0nEater@users.noreply.github.com>
2021-09-07 19:15:00 +1200

    Update Engine class with collision detection

--- TWL/TWL/code/Update.cpp
+++ TWL/TWL/code/Update.cpp
@@ -20,6 +20,35 @@ void Engine::update(float dtAsSeconds)
 		// Update Bob
 		m_Bob.update(dtAsSeconds);
 
+		// Detect collisions and see if characters
+		// have reached the goal tile
+		// The second part of the if condition is only executed
+		// when thomas is touching the home tile
+		if (detectCollisions(m_Thomas) && detectCollisions(m_Bob))
+		{
+			// New level required
+			m_NewLevelRequired = true;
+
+			// Play the reach goal sound
+
+		}
+		else
+		{
+			// Run bobs collision detection
+			detectCollisions(m_Bob);
+		}
+
+		// Let bob and thomas jump on each others heads
+		if (m_Bob.getFeet().intersects(m_Thomas.getHead()))
+		{
+			m_Bob.stopFalling(m_Thomas.getHead().top);
+		}
+		else if (m_Thomas.getFeet().intersects(m_Bob.getHead()))
+		{
+			m_Thomas.stopFalling(m_Bob.getHead().top);
+		}
+
+
 		// Count down the time the player has left
 		m_TimeRemaining -= dtAsSeconds;
 
