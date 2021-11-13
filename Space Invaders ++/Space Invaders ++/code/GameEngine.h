d7ec517bd0773c695c09f79f22438368bca085d0
Nick Nicholas <thenickys123@gmail.com>
2021-11-13 18:11:41 +1300

    Added GameEnginer class, a class for debugging and the main class

---
+++ Space Invaders ++/Space Invaders ++/code/GameEngine.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include "ScreenManager.h"
+#include "SoundEngine.h"
+
+using namespace sf;
+using namespace std;
+
+class GameEngine {
+private:
+	Clock m_Clock;
+	Time m_DT;
+	RenderWindow m_Window;
+
+	unique_ptr<ScreenManager> m_ScreenManager;
+
+	float m_FPS = 0;
+	Vector2f m_Resolution;
+
+	void handleInput();
+	void update();
+	void draw();
+
+public:
+	SoundEngine m_SoundEngine;
+
+	GameEngine();
+	void run();
+};
