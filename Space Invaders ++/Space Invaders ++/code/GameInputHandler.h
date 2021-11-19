d1e73b6db1b6123edaa65922098ba06f87930bd7
Nick Nicholas <thenickys123@gmail.com>
2021-11-19 14:18:45 +1300

    Add GameInputHandler class

---
+++ Space Invaders ++/Space Invaders ++/code/GameInputHandler.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "InputHandler.h"
+
+class GameScreen;
+class GameInputHandler : public InputHandler
+{
+public:
+	void initialize();
+	void handleGamepad() override;
+	void handleKeyPressed(Event& event,
+		RenderWindow& window) override;
+
+	void handleKeyReleased(Event& event,
+		RenderWindow& window) override;
+};
