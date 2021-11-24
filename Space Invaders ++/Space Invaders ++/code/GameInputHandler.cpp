0803c2324e753c9447fe24d1a0d77d69cc82d3b6
Nick Nicholas <thenickys123@gmail.com>
2021-11-24 15:21:48 +1300

    Update GameInputHandler class, add keyboard inputs

--- Space Invaders ++/Space Invaders ++/code/GameInputHandler.cpp
+++ Space Invaders ++/Space Invaders ++/code/GameInputHandler.cpp
@@ -2,6 +2,8 @@
 #include "SoundEngine.h"
 #include "GameScreen.h"
 
+class BulletSpawner;
+
 void GameInputHandler::initialize()
 {
 }
@@ -20,8 +22,60 @@ void GameInputHandler::handleKeyPressed(
 		getPointerToScreenManagerRemoteControl()->
 			SwitchScreens("Select");
 	}
+
+	if (event.key.code == Keyboard::Left)
+	{
+		m_PUC->moveLeft();
+	}
+
+	if (event.key.code == Keyboard::Right)
+	{
+		m_PUC->moveRight();
+	}
+
+	if (event.key.code == Keyboard::Up)
+	{
+		m_PUC->moveUp();
+	}
+
+	if (event.key.code == Keyboard::Down)
+	{
+		m_PUC->moveDown();
+	}
 }
+
 void GameInputHandler::handleKeyReleased(
 	Event& event, RenderWindow& window)
 {
+	if (event.key.code == Keyboard::Left)
+	{
+		m_PUC->stopLeft();
+	}
+
+	else if (event.key.code == Keyboard::Right)
+	{
+		m_PUC->stopRight();
+	}
+
+	else if (event.key.code == Keyboard::Up)
+	{
+		m_PUC->stopUp();
+	}
+
+	else if (event.key.code == Keyboard::Down)
+	{
+		m_PUC->stopDown();
+	}
+
+	else if (event.key.code == Keyboard::Space)
+	{
+		// Shoot a bullet
+		SoundEngine::playShoot();
+		Vector2f spawnLocation;
+		spawnLocation.x = m_PTC->getLocation().x + m_PTC->getSize().x / 2;
+		spawnLocation.y = m_PTC->getLocation().y;
+
+		static_cast<GameScreen*>(getmParentScreen())->
+			spawnBullet(spawnLocation, true);
+	}
 }
--- Space Invaders ++/Space Invaders ++/code/GameInputHandler.h
+++ Space Invaders ++/Space Invaders ++/code/GameInputHandler.h
@@ -1,9 +1,17 @@
 #pragma once
 #include "InputHandler.h"
+#include "PlayerUpdateComponent.h"
+#include "TransformComponent.h"
 
 class GameScreen;
 class GameInputHandler : public InputHandler
 {
+private:
+	shared_ptr<PlayerUpdateComponent> m_PUC;
+	shared_ptr<TransformComponent> m_PTC;
+
+	bool mBButtonPressed = false;
+
 public:
 	void initialize();
 	void handleGamepad() override;
